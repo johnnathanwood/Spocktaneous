6a65cf412cef18bf3e23c3e7ddbc3018372fb7a5
John Wood <johnnathanwood@gmail.com>
2018-09-12 10:52:12 -0500

    working on trip form

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -35,7 +35,7 @@ export default class Login extends Component {
 
             if (users) {
                 localStorage.setItem("credentials", JSON.stringify(users))
-                document.location.href = 'http://localhost:3000/Home'
+                document.location.href = 'http://localhost:3000/trip'
             } else {
                 alert("You need to register")
                 document.location.href = 'http://localhost:3000/register'
--- src/components/trip/TripPageForm.js
+++ src/components/trip/TripPageForm.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react'
 
-export default class TripPageForm extends Component {
+export default class MessageForm extends Component {
 
-    findTrip = trip => {
-        return this.props.trip.find(trip => trip.id === trip.tripId).tripId
-    }
 
-    // Set initial state
+    //set initial state here
+
+
     state = {
-        tripId: "",
-        tripBudget: "",
-        tripNumber: "",
-        userId: "",
+        messageId: "",
+        messageTo: "",
+        message: "",
+        messageDate: ""
     }
 
     handleFieldChange = evt => {
@@ -20,56 +19,57 @@ export default class TripPageForm extends Component {
         this.setState(stateToChange)
     }
 
-    constructTrip = evt => {
+    constructNewMessage = evt => {
         evt.preventDefault()
         const credentials = JSON.parse(localStorage.getItem('credentials'))
-        const trip = {
-            id: this.state.tripId,
-            budget: this.state.tripBudget,
-            number: this.state.tripNumber,
+        const messages = {
+            id: this.state.messageId,
+            to: this.state.messageTo,
+            message: this.state.message,
+            date: new Date().toISOString(),
             userId: credentials.id
         }
 
         this.props.addTrip(trip)
-        .then(() => this.props.history.push("/"))
+        .then(() => this.props.history.push("/trip"))
     }
 
     render() {
 
         return (
             <React.Fragment>
-                <h2>Create a Spocktaneous adventure</h2>
-                <form className="tripPageForm">
+                <h2>Message</h2>
+                <form className="messageForm">
                     <div className="form-group">
-                        <section className="tripField">
-                            <label htmlFor="tripId"></label>
-                            <select htmlFor="tripBudget">Budget $$</select>
+                        <section className="messageField">
+                            <label htmlFor="messageId"></label>
+                            <label htmlFor="messageTo">Message To:</label>
                             <p></p>
-                            <select type="text" required="true"
+                            <input type="text" required="true"
                                 className="form-control"
                                 onChange={this.handleFieldChange}
-                                id="trip-budget"
-                                placeholder="Budget"
-                                size="auto"/>
+                                id="messageTo"
+                                placeholder="Message To"
+                                size="35" />
                         </section>
                     </div>
                     <p></p>
                     <div className="form-group">
                     </div>
                     <div className="form-group">
-                        <section  type="number"className="tripField">
+                        <section className="messageField">
                             <p></p>
-                            <label htmlFor="number">How many?</label>
+                            <label htmlFor="message">Message:</label>
                             <p></p>
                             <textarea
                                 className="form-control"
                                 onChange={this.handleFieldChange}
-                                id="trip"
+                                id="message"
                                 placeholder="..." rows="5" cols="70"></textarea>
                         </section>
                     </div>
                     <p></p>
-                    <button type="submit" onClick={this.constructTrip} className="btn btn-primary"></button>
+                    <button type="submit" onClick={this.constructNewMessage} className="btn btn-primary">Send Message</button>
                 </form>
             </React.Fragment>
         )
