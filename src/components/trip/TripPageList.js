d4823e60b3f260c8cf8a51c6ad1d8d7447318237
John Wood <johnnathanwood@gmail.com>
2018-09-14 15:48:33 -0500

    fixed more bugs. Moving right along

--- src/components/trip/TripPageList.js
+++ src/components/trip/TripPageList.js
@@ -8,17 +8,13 @@ export default class TripPageList extends Component {
             
             <section>
                 <section>
-                    {
-                this.props.trips.map(trip =>
-                <div id={`trips--${trip.id}`} key={trip.id} className="tripCard">
+                    
                 <header>Hello{this.props.trip.name}</header>
                 <fieldset>
                 <p>Location:{this.props.trip.location}</p>
                 <p>Date:{this.props.trip.date}</p>
                 <p>Price:{this.props.trip.price}</p>
                 </fieldset>
-                </div>
-                )}
                 </section>
             </section>
         )
